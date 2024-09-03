86b00112d93231b23f1ad493ec7ce2db87af1b39
MariaGLi <lizrazoleal27@gmail.com>
2024-09-02 19:50:24 -0500

    :construction: En proceso.

--- Dia9/script.js
+++ Dia9/script.js
@@ -2,16 +2,18 @@ let url = `https://pokeapi.co/api/v2/pokemon/`;
 
 document.getElementById("holder").addEventListener('input',(e)=>{
     var search = document.getElementById("name").value;
-    console.log(search);
     
     fetch(url+search)
     .then(res => res.json())
     .then(data =>{
-        console.log(data);
         var cont = document.getElementById("cont2");
 
         cont.innerHTML=`
+        <p>${data.id}
         <img id="gifs" src="${data.sprites.other.showdown.front_default}">
-        `        
+        `
+
     });
+
+
 });
