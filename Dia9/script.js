649f3f19763f17d6e78d1129c1424d26046d58a4
MariaGLi <lizrazoleal27@gmail.com>
2024-09-04 18:48:50 -0500

    :construction: PokeAPI.

--- Dia9/script.js
+++ Dia9/script.js
@@ -1,47 +1,18 @@
 let url = `https://pokeapi.co/api/v2/pokemon/`;
 
-function poke(){
-    document.getElementById("holder").addEventListener('input',(e)=>{
-        var search = document.getElementById("name").value;
-        fetch(url + search)
-        .then(res => res.json())
-        .then(data =>{
-            var cont = document.getElementById("cont2");
-            
+
+document.getElementById("holder").addEventListener('input',(e)=>{
+    var search = document.getElementById("name").value;
+    fetch(url + search)
+    .then(res => res.json())
+    .then(data =>{
+        var cont = document.getElementById("cont2");
         
-    
-            cont.innerHTML=`
-            <p id="idecito">${data.id}</p>
-            <p id="nombre">${data.name}</p>
-            <img id="gifs" src="${data.sprites.other.showdown.front_default}">
-            `
-
-            let idPokemon = 0
-
-            document.getElementById("prev").addEventListener("click", (e) =>{
-                idPokemon = data.id - 1
-                search = String(idPokemon)
-                
-            })
-            poke()
-
-           /* document.getElementById("prev").addEventListener("click", (e) => {
-                idPokemon = data.id - 1
-                search = String(idPokemon)
-                console.log(search);
-                
-                poke()
-            })*/
-            
-            document.getElementById("next").addEventListener("click",(e)=>{
-                idPokemon = data.id + 1
-                search = String(idPokemon)
-                console.log(search);
-                
-                poke()
-            })
-        });
+        cont.innerHTML=`
+        <p id="idecito">${data.id}</p>
+        <p id="nombre">${data.name}</p>
+        <img id="gifs" src="${data.sprites.other.showdown.front_default}">
+        `
     });
-}
-poke()
+});
 
